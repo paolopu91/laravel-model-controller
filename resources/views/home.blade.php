9661ed15fa48dcb2753b5f65d6fe585d5b4f8207
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-28 16:24:14 +0200

    added bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,17 @@
 @section('page_title', "Movies")
     
 @section('page_content')
-    <div class="section-movie bg-info">
+    <div class="section-movie bg-dark">
         <div class="container">
             
                 
                 @foreach ($movies as $movie)
-                    <div class="text-center"> 
-                       <h4><span>Title:</span>  <em class="text-danger">{{ $movie["title"] }}</em></h4> 
-                       <h4><span>Original Title:</span> <em class="">{{$movie["original_title"]}}</em></h4>
-                        <h4><span>Nationality :</span> <strong class="">{{$movie["nationality"]}}</strong></h4> 
-                       <h4><span>Date :</span>  <em>{{$movie["date"]}}</em></h4>
-                       <h4><span>Vote:</span>  <strong>{{$movie["vote"]}}</strong></h4>
+                    <div class="text-center py-5 text-white"> 
+                       <h4><span class="text-success">Title:</span>  <em class="text-danger">{{ $movie["title"] }}</em></h4> 
+                       <h4><span class="">Original Title:</span> <em class="">{{$movie["original_title"]}}</em></h4>
+                        <h4><span class="">Nationality :</span> <strong class="">{{$movie["nationality"]}}</strong></h4> 
+                       <h4><span class="">Date :</span>  <em>{{$movie["date"]}}</em></h4>
+                       <h4><span class="">Vote:</span>  <strong class="text-success">{{$movie["vote"]}}</strong></h4>
                     </div>
                 @endforeach
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,7 @@
-<header>
-
-    <h1 class="text-center pt-5 text-primary">LA LISTA DEI MIEI FILM</h1>
-
+<header >
+    <div class="bg-dark">
+        <div class="container">
+            <h1 class="text-center pt-5 text-primary">LA LISTA DEI MIEI FILM</h1>
+        </div>
+    </div>
 </header>
