b764809bcf84f7adeda4c2f7b0f8ee409b9c2a00
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-28 16:35:41 +0200

    added bonus card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,22 @@
 @section('page_content')
     <div class="section-movie bg-dark">
         <div class="container">
-            
-                
+            <div class="row">
                 @foreach ($movies as $movie)
-                    <div class="text-center py-5 text-white"> 
+                <div class="col-4">
+                    <div class="text-center py-5 text-white px-3"> 
                        <h4><span class="text-success">Title:</span>  <em class="text-danger">{{ $movie["title"] }}</em></h4> 
                        <h4><span class="">Original Title:</span> <em class="">{{$movie["original_title"]}}</em></h4>
                         <h4><span class="">Nationality :</span> <strong class="">{{$movie["nationality"]}}</strong></h4> 
                        <h4><span class="">Date :</span>  <em>{{$movie["date"]}}</em></h4>
                        <h4><span class="">Vote:</span>  <strong class="text-success">{{$movie["vote"]}}</strong></h4>
                     </div>
+                </div>
+                    
                 @endforeach
+            </div>
+                
+               
 
            
         </div>
